88440b5caa1a1a3f9f92d911e0b6fcd3e1601ce2
Vinu <vinujithmin@gmail.com>
2024-03-01 14:59:05 +1100

    changes per review

---
+++ client/src/components/NoteBuilder/NoteBuilder.styled.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 50%;
+  border: 1px solid grey;
+  padding: 20px;
+  margin: 20px;
+  border-radius: 8px;
+`
+
+const List = styled.li`
+  list-style-type: none;
+  border: 1px solid grey;
+  padding: 16px;
+  width: 100%;
+  border-radius: 8px;
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
+  &:not(:first-child) {
+    margin-bottom: 16px;
+  }
+  span {
+    color: grey;
+  }
+`
+
+const Controls = styled.div`
+  display: flex;
+  margin-left: auto;
+  gap: 8px;
+  svg.CHEVRON {
+    transform: rotate(180deg);
+    path {
+      fill: grey;
+    }
+  }
+`
+
+export { Container, List, Controls }

1fb2e52450c8a0e2d6890515cc45f766a38b1fc3
Vinu <vinujithmin@gmail.com>
2024-03-07 11:35:40 +1100

    initial start search history modal

---
+++ client/src/components/NoteBuilder/Search/SearchHistory.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const SearchModal = () => {
+  return (
+    <Container>
+      <ContainerBody>
+        <h3>Recent</h3>
+        <SearchGroup>
+          <SearchItem></SearchItem>
+        </SearchGroup>
+      </ContainerBody>
+    </Container>
+  )
+}
+
+export { SearchModal }
